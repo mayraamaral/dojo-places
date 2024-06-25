f2792033ab9b347bff3e5ec0a30bcd108491b2f3
Mayra Amaral <mayracunha18@gmail.com>
2024-06-25 16:45:15 -0300

    chore: adiciona método que retorna um local pelo código

--- src/main/java/br/com/alura/dojoplaces/repository/LocalRepository.java
+++ src/main/java/br/com/alura/dojoplaces/repository/LocalRepository.java
@@ -4,6 +4,9 @@ import br.com.alura.dojoplaces.model.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LocalRepository extends JpaRepository<Local, Long> {
+    Optional<Local> findByCodigo(String codigo);
 }
